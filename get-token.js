94c0e290c65ae7d9247abe89f9b851914404957c
brokerinsight <martinngothokeli@gmail.com>
2025-04-13 00:23:48 +0300

    Update and rename get-token.js to non

--- get-token.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  const githubToken = process.env.GITHUB_TOKEN;
-  if (!githubToken) {
-    return res.status(500).json({ error: "GitHub token not configured" });
-  }
-  res.status(200).json({ token: githubToken });
-};
